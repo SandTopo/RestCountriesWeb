0e7331d1b0f7da578a104600e7d89c2bf4129e8a
SandTopo <guillermito356789@gmail.com>
2024-02-27 17:02:11 +0100

    Initial commit

---
+++ src/main/java/com/ceica/restcountriesweb/HelloServlet.java
@@ -0,0 +1,31 @@
+package com.ceica.restcountriesweb;
+
+import java.io.*;
+
+import com.ceica.restcountriesweb.Services.RestCountriesService;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "countries", value = "/countries")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        RestCountriesService service=new RestCountriesService();
+        String[] regions= service.getRegions();
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + regions[0] + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
